c9d87dd0fe9e71d0da1876431a721faa0acc6514
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-08-25 12:37:53 -0500

    Update informacion.php

--- Web/login/Login 01/informacion.php
+++ Web/login/Login 01/informacion.php
@@ -5,7 +5,6 @@ $nombres=$_POST["nombres"];
 $Apellidos=$_POST["Apellidos"];
 $email=$_POST["email"];
 $Genero=isset($_POST["Genero"])?implode(",",$_POST["Genero"]):"";
-$poblacion=$_POST["poblacion"];
 $descripcion=$_POST["descripcion"];
 
 //almecennar
@@ -13,8 +12,7 @@ $_SESSION["nombres"]=$nombres;
 $_SESSION["Apellidos"]=$Apellidos;
 $_SESSION["email"]=$email;
 $_SESSION["Genero"]=$Genero;
-$_SESSION["poblacion"]=$poblacion;
-$_SESSION["descripcion"]=$descripcion
+$_SESSION["descripcion"]=$descripcion;
 
 //redireccion a la pagina de visualizacion
 header("Location:mostrar.php");
